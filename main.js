233bf14278e99ac6ce6cb00e639cbf5abf6ad214
Prazma Repos <tikitikiteke.newgrounds@gmail.com>
2017-11-05 22:48:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-        		var camera, scene, renderer, controls, floor, saber;
-			var centerLayer, oneLayer, twoLayer, threeLayer;
+var camera, scene, renderer, controls, floor, saberPointer;
+			var centerLayer, oneLayer, twoLayer, threeLayer, shooterMach;
+            var saberOriginX, saberOriginY;
 			var geometry, mesh, meshL1, meshL2, meshL3;
 			var objects = [];
 			var raycaster;
@@ -18,17 +19,33 @@
 			var velocity = new THREE.Vector3();
 			var direction = new THREE.Vector3();
 			function init() {
+                saberOriginX = 0;
+                saberOriginY = 0;
 				camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 100000 );
 				camera.position.z = 3;
 				camera.position.y = 10;
 				scene = new THREE.Scene();
-				scene.background = new THREE.Color( 0xffffff );
+				scene.background = new THREE.Color( 0x000000 );
 				scene.fog = new THREE.Fog( 0xF0FBFF, 0, 2000 );
-                var saberGeo = new THREE.BoxGeometry( 0.6, 10, 0.6 );
-                var saberMat = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-                saber = new THREE.Mesh( saberGeo, saberMat );
-                saber.position.z = 990;
-				var light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
+                
+                var pointerG = new THREE.BoxGeometry( 10, 10, 10 );
+                var pointerM = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+                saberPointer = new THREE.Mesh( pointerG, pointerM );
+                scene.add( saberPointer );
+                
+                var shooterMachG = new THREE.BoxGeometry( 3, 10, 3 );
+                var shooterMachM = new THREE.MeshBasicMaterial( { color: 0xbfbfbf } );
+                shooterMach = new THREE.Mesh( shooterMachG, shooterMachM );
+                scene.add( shooterMach );
+                shooterMach.position.z = -50;
+                shooterMach.position.y = 10;
+                
+                var ambient = new THREE.AmbientLight( 0x444444 );
+				scene.add( ambient );
+				var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+				directionalLight.position.set( 0, 0, 1 ).normalize();
+				scene.add( directionalLight );
+                var light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
 				light.position.set( 0.5, 1, 0.75 );
 				scene.add( light );
 				raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
@@ -38,6 +55,43 @@
 				floor = new THREE.Mesh( floorGeometry, floorMaterial );
 				scene.add( floor );
                 floor.rotation.x = Math.PI / -2;
+                
+                	geometry = new THREE.ConeGeometry( 0.25, 15 );
+                geometry.applyMatrix( new THREE.Matrix4().makeTranslation(0, 7, 0) );
+				centerLayer = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+				oneLayer = new THREE.MeshBasicMaterial( { color: 0x03A9F4, transparent: true, opacity: 0.3 } );
+				twoLayer = new THREE.MeshBasicMaterial( { color: 0x03A9F4, transparent: true, opacity: 0.2 } );
+				threeLayer = new THREE.MeshBasicMaterial( { color: 0x03A9F4, transparent: true, opacity: 0.18 } );
+
+				mesh = new THREE.Mesh( geometry, centerLayer );
+				scene.add( mesh );
+				
+				geometry = new THREE.ConeGeometry( 0.35, 15.1 );
+                geometry.applyMatrix( new THREE.Matrix4().makeTranslation(0, 7, 0) );
+				meshL1 = new THREE.Mesh( geometry, oneLayer );
+				scene.add( meshL1 );
+
+				
+				geometry = new THREE.ConeGeometry( 0.45, 15.2 );
+                geometry.applyMatrix( new THREE.Matrix4().makeTranslation(0, 7, 0) );
+				meshL2 = new THREE.Mesh( geometry, twoLayer );
+				scene.add( meshL2 );
+				
+				
+				geometry = new THREE.ConeGeometry( 0.55, 15.3 );
+                geometry.applyMatrix( new THREE.Matrix4().makeTranslation(0, 7, 0) );
+				meshL3 = new THREE.Mesh( geometry, threeLayer );
+				scene.add( meshL3 );
+                
+				mesh.position.y = 5;
+				meshL1.position.y = 5;
+				meshL2.position.y = 5;
+				meshL3.position.y = 5;
+				mesh.position.z = 7;
+				meshL1.position.z = 7;
+				meshL2.position.z = 7;
+				meshL3.position.z = 7;
+                
 				// objects
                 scene.add( makeSkybox( [
 				    'https://raw.githubusercontent.com/Prazma/xfinity/master/map/right.png', // right
@@ -72,35 +126,17 @@
 						uniforms : skyboxShader.uniforms, depthWrite : false, side : THREE.BackSide
 					})
 				);
-		    		geometry = new THREE.ConeGeometry( 0.05, 2 );
-				centerLayer = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-				oneLayer = new THREE.MeshBasicMaterial( { color: 0x03A9F4, transparent: true, opacity: 0.3 } );
-				twoLayer = new THREE.MeshBasicMaterial( { color: 0x03A9F4, transparent: true, opacity: 0.2 } );
-				threeLayer = new THREE.MeshBasicMaterial( { color: 0xffffff, transparent: true, opacity: 0.18 } );
-
-				mesh = new THREE.Mesh( geometry, centerLayer );
-				scene.add( mesh );
-				
-				geometry = new THREE.ConeGeometry( 0.08, 2.01 );
-				meshL1 = new THREE.Mesh( geometry, oneLayer );
-				scene.add( meshL1 );
-
-				
-				geometry = new THREE.ConeGeometry( 0.1, 2.02 );
-				meshL2 = new THREE.Mesh( geometry, twoLayer );
-				scene.add( meshL2 );
-				
-				
-				geometry = new THREE.ConeGeometry( 0.12, 2.03 );
-				meshL3 = new THREE.Mesh( geometry, threeLayer );
-				scene.add( meshL3 );
-				
-				mesh.position.y = 10;
-				meshL1.position.y = 10;
-				meshL2.position.y = 10;
-				meshL3.position.y = 10;
+                document.canvas.requestPointerLock();
 			}
 			function animate() {
+                saberPointer.position.z = -40;
+                saberPointer.position.x = saberOriginX;
+                saberPointer.position.y = saberOriginY;
+                camera.position.z = 20;
 				renderer.render( scene, camera );
 				requestAnimationFrame( animate );
 			}
+            function saberOrigin(e) {
+                saberOriginX = e.clientX;
+                saberOriginY = e.clientY;
+            }
